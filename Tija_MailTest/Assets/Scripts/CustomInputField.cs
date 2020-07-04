656243e6707352d6ffbf64883528acd0847a0094
Matthieu Heras <37445049+MatthieuHeras@users.noreply.github.com>
2020-07-04 13:52:56 +0200

    Core mechanic

---
+++ Tija_MailTest/Assets/Scripts/CustomInputField.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CustomInputField : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI placeholder = default;
+        [SerializeField] private TextMeshProUGUI text = default;
+
+        public event Action OnCompletion = delegate {  };
+
+        private string currentText = "";
+        private string currentWord = "";
+        private string newWord = "";
+        
+        private bool isActivated = false;
+
+        private void Update()
+        {
+            if (!isActivated)
+                return;
+
+            string input = Input.inputString;
+            input = WordDatabase.FilterString(input);
+            
+            if (input == "")
+                return;
+            
+            currentWord += input;
+
+            if (currentWord.Length > newWord.Length)
+                currentWord = currentWord.Substring(0, newWord.Length);
+
+            if (currentWord.Length == newWord.Length)
+                CompleteWord();
+            
+            RefreshText();
+            RefreshPlaceholder();
+        }
+
+        public void AddWord(string word)
+        {
+            newWord = word;
+            RefreshPlaceholder();
+        }
+
+        public void Activate()
+        {
+            isActivated = true;
+        }
+
+        private void RefreshText()
+        {
+            text.text = currentText + currentWord;
+        }
+
+        private void RefreshPlaceholder()
+        {
+            placeholder.text = currentText + currentWord + newWord.Substring(currentWord.Length);
+        }
+
+        private void CompleteWord()
+        {
+            currentText += currentWord + " ";
+            currentWord = "";
+            newWord = "";
+            OnCompletion();
+        }
+    }
+}
---
+++ Tija_MailTest/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private CustomInputField inputField = default;
+    
+    private bool hasStarted = false;
+
+    private void Update()
+    {
+        if (hasStarted)
+            return;
+        //if (Input.GetKeyDown(KeyCode.Space))
+        
+            hasStarted = true;
+            inputField.AddWord(WordDatabase.GetRandomWord());
+            inputField.Activate();
+            inputField.OnCompletion += HandleWordDone;
+        
+    }
+    private void HandleWordDone()
+    {
+        Debug.Log("word done !");
+        inputField.AddWord(WordDatabase.GetRandomWord());
+    }
+
+    private void OnDisable()
+    {
+        inputField.OnCompletion -= HandleWordDone;
+    }
+}
